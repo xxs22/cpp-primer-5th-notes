ae781b15f31e859ed8ca21f6c1c532f65ced66b0
xxs22 <xiansong.xxs@gmail.com>
2022-08-24 14:55:05 +0800

    :tada: feat(02): 2.2/2.3 variables & compound type

--- Chap01:Getting Started/test1.cpp
+++ Chap01:Getting Started/test1.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
 #include <cstdio>
+
+inline const char *const BoolToString(bool b)
+{
+  return b ? "true" : "false";
+}
 
 int main()
 {
@@ -9,5 +15,19 @@ int main()
     ++val;
   }
   std::printf("Sum of 1 to 10 inclusive is %d\n", sum);
+
+  int *p1 = &sum, *p2 = p1;
+  std::printf("*p1 equals *p2 is %s\n", BoolToString(p1 == p2));
+
+  // 打印指针地址
+  // std::printf("*p1 addrs is:%d, *p2 addrs is:$%d\n", p1, p2);
+
+  p2++; // 指针指向下一个地址
+  // std::printf("*p1 addrs is:%d, *(p2++) addrs is:$%d\n", p1, p2);
+
+  // 指针指向下一个地址是否相等
+  std::printf("*p1 equals *(p2 + 1) is %s\n", BoolToString(p1 == p2));
+  // 指针指向下一个地址值是否相等
+  std::printf("*p1 value is %d, *(p2 + 1) value is %d\n", *p1, *p2);
   return 0;
 }
